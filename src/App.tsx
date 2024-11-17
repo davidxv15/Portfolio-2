efe5d4634b2426a6ab61bcfa36a3399074eff0fc
davidxv15 <davidxvaz@gmail.com>
2024-11-16 23:26:50 -0700

    App.tsx test

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ const App: React.FC = () => {
   return (
     <div>
       <h1>Portfolio for me</h1>
-      <h6>Portfolio for me</h6>
+      <h2>Portfolio for me</h2>
       <p>content</p>
     </div>
   );
