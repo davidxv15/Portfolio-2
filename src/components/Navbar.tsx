649d3b606b4e3264e287b78e5dd7a36406d7cf91
davidxv15 <davidxvaz@gmail.com>
2024-11-17 23:35:12 -0700

    flex div: h-10

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar: React.FC = () => {
   return (
     <nav className="bg-gray-800 text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
+        <div className="flex items-center justify-between h-10">
           {/* Logo or Brand Name */}
           <div className="flex items-center">
             <a href="#" className="text-2xl font-bold">
