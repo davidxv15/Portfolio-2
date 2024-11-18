bb32f9281dead549b3a26bf0176ead1d1c1de887
davidxv15 <davidxvaz@gmail.com>
2024-11-17 23:17:13 -0700

    import Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import HeroSection from './components/HeroSection';
+import Navbar from './components/Navbar';
 
 const App: React.FC = () => {
   return (
     <div>
       <HeroSection />
+      <Navbar />
     </div>
   );
 };
