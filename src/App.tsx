d5804d4c3d2580f2e2e1571216e30d0b59150dd0
davidxv15 <davidxvaz@gmail.com>
2024-11-17 22:59:38 -0700

    add HeroSection

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import HeroSection from './components/HeroSection';
 const App: React.FC = () => {
   return (
     <div>
+      <HeroSection />
       <h1>Portfolio for me</h1>
       <h2>Portfolio for me</h2>
       <p>content</p>
