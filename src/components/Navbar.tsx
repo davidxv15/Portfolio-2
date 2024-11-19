5f0a29a6f2f7079bbef9d2caaa97036c7df61c49
davidxv15 <davidxvaz@gmail.com>
2024-11-18 21:50:50 -0700

    Name: text-gray-400

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar: React.FC = () => {
   return (
     <nav className="bg-gray-800 text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-10">
+        <div className="text-gray-400 flex items-center justify-between h-10">
           David Velasquez
           <div className="flex items-center">
             <a href="#" className="text-gray-400 hover:text-gray-300 text-2xl font-bold">
