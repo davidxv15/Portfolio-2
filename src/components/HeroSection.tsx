a83f005fb261c284b0a3dd3b8dcc82a3f317d51f
davidxv15 <davidxvaz@gmail.com>
2024-11-20 00:05:25 -0700

    Photo: object & shadow

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 const HeroSection: React.FC = () => {
   return (
-    <section className="flex flex-col items-center justify-center h-screen bg-blue-400 text-center">
+    <section className="flex flex-col items-center justify-center h-screen bg-blue-200 text-center">
       <div className="mb-6">
         <h1 className="text-5xl font-bold text-gray-800">
           Display Brand value immediately
@@ -16,7 +16,7 @@ const HeroSection: React.FC = () => {
         <img
           src="/WhiteShirtPhoto.jpg"
           alt="My Portrait"
-          className="rounded-full w-64 h-64 object-cover object-[80%_8%] shadow-lg"
+          className="rounded-full w-64 h-64 object-cover object-[80%_8%] shadow-lg shadow-gray-600"
         />
       </div>
     </section>
