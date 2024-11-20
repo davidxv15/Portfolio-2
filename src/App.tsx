f67635e0a910fa7d9407c888761accb661a20dce
davidxv15 <davidxvaz@gmail.com>
2024-11-19 22:47:28 -0700

    App: auto-Theme Names

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App: React.FC = () => {
         <Navbar /></div>
         <div className="mr-auto">
       <ThemeToggle
-          themes={['dark', 'light', 'warm', 'retro']}
+          themes={['dark', 'light', 'invert', 'grayscale', 'retro', 'sepia']}
           onThemeChange={handleThemeChange}
         /></div>
       <HeroSection />
