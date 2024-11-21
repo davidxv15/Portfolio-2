d1daf2863c5871516157cd5ea9e71439ed28388c
davidxv15 <davidxvaz@gmail.com>
2024-11-20 23:42:48 -0700

    add fonts to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}' // Includes all necessary file types in src
   ],
   theme: {
-    extend: {}, // For adding customizations later
+    extend: {
+      fontFamily: {
+        fira: ['Fira Sans Condensed', 'sans-serif'], // Add 'Fira Sans Condensed'
+        poppins: ['Poppins', 'sans-serif'],         // Add 'Poppins'
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'), // Optional: adds better styling for forms
