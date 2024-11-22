442ee8a4df5d1dd21a5c91405affe385ac9609b9
davidxv15 <davidxvaz@gmail.com>
2024-11-21 21:12:22 -0700

    hero: btn: font-outfit

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -20,7 +20,7 @@ const HeroSection: React.FC = () => {
         />
       </div>
       <div className="mt-4">
-        <button className="bg-blue-500 text-white px-6 py-2 rounded-full shadow hover:bg-blue-600 transition">
+        <button className="bg-blue-500 text-white px-6 py-2 rounded-full shadow hover:bg-blue-600 transition font-outfit">
           View My Work
         </button>
         <button className="bg-gray-200 text-blue-500 px-6 py-2 rounded-full shadow hover:bg-gray-300 transition ml-4">
