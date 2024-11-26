5b5f19fb84fc6a9d44cf8cb0037a30d1d7abeced
davidxv15 <davidxvaz@gmail.com>
2024-11-25 22:03:57 -0700

    Nav: font-outfit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar: React.FC = () => {
   return (
     <nav className="bg-gradient-to-b from-blue-800 via-blue-700 to-blue-600 text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="text-gray-100 text-xl font-barlow flex items-center justify-between h-14">
+        <div className="text-gray-100 text-xl font-outfit flex items-center justify-between h-14">
           David Velasquez
           <div className="flex items-center">
             <a
