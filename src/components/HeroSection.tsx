dd7965f1b3ee2b407455375c5ce5d1c99ca870e4
davidxv15 <davidxvaz@gmail.com>
2024-12-02 04:33:42 -0700

    section: md:mt-5

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 const HeroSection: React.FC = () => {
   return (
-    <section className="flex flex-col items-center justify-evenly h-screen w-screen bg-gradient-to-t from-blue-800 via-blue-200 to-blue-200 text-center md:mt-10 lg:mt-0">
+    <section className="flex flex-col items-center justify-evenly h-screen w-screen bg-gradient-to-t from-blue-800 via-blue-200 to-blue-200 text-center md:mt-5 lg:mt-0">
       <div className="mb-2 bg-gradient-to-b from-blue-100 via-blue-100 to-blue-200">
         <h1 className="surname text-5xl font-league font-bold text-slate-900 bg-blue-100 
          w-3/4 mx-auto outline outline-none">
