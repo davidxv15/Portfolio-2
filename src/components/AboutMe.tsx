ce5d4f2860c355f403689b16eaed29faadfbaa24
davidxv15 <davidxvaz@gmail.com>
2024-12-06 22:45:16 -0700

    mb-4

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -5,7 +5,7 @@ const AboutMe: React.FC = () => {
     <section className="bg-gray-100 text-slate-900 py-12 px-4 sm:px-8" id="about">
       <div className="max-w-4xl mx-auto text-center">
         <h2 className="text-3xl sm:text-4xl font-league mb-4">About Me</h2>
-        <p className="text-lg sm:text-xl leading-relaxed mb-6">
+        <p className="text-lg sm:text-xl leading-relaxed mb-4">
         Hello,👋🏼 I'm David Velasquez, a versatile professional with a diverse background. As a Full Stack Web Developer, I specialize in creating engaging and effective user experiences with a strong emphasis on authentic design and brand recognition. I am also passionate about promoting differently-abled accessibility in my work. In addition to my technical skills, I have a rich array of interests and experiences. As a musician, I am a multi-instrumentalist who enjoys singing and recording music. I am also a former EMT and chef, and I continue to contribute to my community as a volunteer. This broad range of interests has helped me identify new opportunities for technology to make a positive impact.
         </p>
         <p className="text-md sm:text-lg leading-relaxed">
