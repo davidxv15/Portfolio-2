b36e144266c4dd67e3f9232b835030d6c6964101
davidxv15 <davidxvaz@gmail.com>
2024-12-09 22:18:00 -0700

    h2: y-6

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -11,7 +11,7 @@ const HeroSection: React.FC = () => {
           <span className="text-5.5xl md:text-7xl lg:text-7xl">D</span>AVID{" "}
           <span className="text-5.5xl md:text-7xl lg:text-7xl">V</span>ELASQUEZ
         </h1>
-        <h2 className="text-5xl font-poppins text-slate-900 mt-1 md:text-left lg:text-center md:mr-32 md:ml-[50%] text-center lg:mr-8 lg:-translate-y-8 lg:w-90 md:whitespace-nowrap md:text-4xl lg:whitespace-nowrap">
+        <h2 className="text-5xl font-poppins text-slate-900 mt-1 md:text-left lg:text-center md:mr-32 md:ml-[50%] text-center lg:mr-8 lg:-translate-y-6 lg:w-90 md:whitespace-nowrap md:text-4xl lg:whitespace-nowrap">
           Software Developer
         </h2>
         <p className="text-xl font-outfit text-slate-900 mt-2 md:text-left lg:text-left md:mr-24 md:ml-[50%] lg:ml-[52%] lg:-translate-y-0">
