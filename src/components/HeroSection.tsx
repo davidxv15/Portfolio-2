adb48812dee4ecbcd3685a9aa6937c1f96b2a6ac
davidxv15 <davidxvaz@gmail.com>
2024-12-09 22:27:19 -0700

    name: y-11

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,7 +6,7 @@ const HeroSection: React.FC = () => {
       <div className="mb-2 bg-gradient-to-b from-blue-100 via-blue-100 to-blue-100">
         <h1
           className="surname text-5xl md:text-6xl lg:text-7xl font-league font-bold text-slate-900 bg-transparent 
-         w-3/4 mx-auto outline outline-none sm:-translate-y-12 md:-translate-y-4 lg:-translate-y-14"
+         w-3/4 mx-auto outline outline-none sm:-translate-y-12 md:-translate-y-4 lg:-translate-y-11"
         >
           <span className="text-5.5xl md:text-7xl lg:text-7xl">D</span>AVID{" "}
           <span className="text-5.5xl md:text-7xl lg:text-7xl">V</span>ELASQUEZ
