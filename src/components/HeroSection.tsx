dc5eda2c5ca4dae200d5c263c4de243da1f0796d
davidxv15 <davidxvaz@gmail.com>
2024-12-10 23:38:54 -0700

    lg: pic

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -23,7 +23,7 @@ const HeroSection: React.FC = () => {
         <img
           src="/WhiteShirtPhoto.jpg"
           alt="My Portrait"
-          className="rounded-full w-72 h-72 lg:w-96 lg:h-96 object-cover object-[80%_6%] shadow-lg shadow-gray-600 md:absolute md:top-48 md:left-24 lg:left-28 xl:left-56 lg:top-44 lg:-translate-y-24 xl:top-60 transition-all duration-900 ease-in-out animate-fade"
+          className="rounded-full w-72 h-72 lg:w-96 lg:h-96 object-cover object-[80%_6%] shadow-lg shadow-gray-600 md:absolute md:top-48 md:left-24 lg:left-28 xl:left-56 lg:top-48 lg:-translate-y-24 xl:top-60 transition-all duration-900 ease-in-out animate-fade"
         />
       </div>
       <div className="mt-4 md:ml-96 md:mb-20 lg:space-x-12">
