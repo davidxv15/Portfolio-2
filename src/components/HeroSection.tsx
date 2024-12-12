4bd1ec6e99805f7bee286bc2b33c5f8b5bcb951d
davidxv15 <davidxvaz@gmail.com>
2024-12-11 22:04:27 -0700

    btn spacing

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -57,7 +57,7 @@ const HeroSection: React.FC = () => {
             src="linkedEdit.PNG"
             alt="LinkedIn logo"
             id="linkedin"
-            className="w-14 h-14 lg:w-20 lg:h-20 rounded-full object-cover shadow-md shadow-gray-600 object-cover bg-white text-red-500 md:-translate-y-32 border-white border-2 hover:scale-125 transform transition duration-500"
+            className="w-14 h-14 lg:w-20 lg:h-20 rounded-full object-cover shadow-md shadow-gray-600 object-cover bg-white text-red-500 md:-translate-y-32 md:translate-x-2 border-white border-2 hover:scale-125 transform transition duration-500"
           />
         </a>
       </div>
