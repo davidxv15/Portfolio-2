0c40905d8c627c116b154e62ef6533893dd46d0a
davidxv15 <davidxvaz@gmail.com>
2024-12-16 23:18:37 -0700

    nav: tracking

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar: React.FC = () => {
             </a>
           </div>
           {/* Desktop Navigation Links */}
-          <div className="hidden md:flex space-x-5 tracking-wide">
+          <div className="hidden md:flex space-x-5 tracking-tight">
             <a href="#" className="text-gray-100 hover:text-gray-400">
               Home
             </a>
