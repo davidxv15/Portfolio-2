c2ba448a8c8d61dd5cb93fc45364780bbc11cd6a
davidxv15 <davidxvaz@gmail.com>
2024-12-18 22:41:00 -0700

    btns: hover

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -64,7 +64,7 @@ const Projects: React.FC = () => {
                   href={project.github}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="bg-gradient-to-b from-slate-300 via-slate-100 to-slate-400 text-slate-600 px-4 py-2 rounded-full hover:bg-gray-300 shadow shadow-md shadow-slate-800"
+                  className="bg-gradient-to-b from-slate-300 via-slate-100 to-slate-400 text-slate-600 px-4 py-2 rounded-full hover:bg-gradient-to-b from-slate-400 to-slate-200 hover:text-slate-600 shadow shadow-md shadow-slate-800"
                 >
                   GitHub Repository
                 </a>
