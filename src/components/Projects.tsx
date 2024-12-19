aad0fe54b571b1e75cf94750a33ec5a262fd95d7
davidxv15 <davidxvaz@gmail.com>
2024-12-18 22:33:47 -0700

    metal btn

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -64,7 +64,7 @@ const Projects: React.FC = () => {
                   href={project.github}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="bg-gradient-to-b from-slate-300 via-slate-100 to-slate-400 text-slate-900 px-4 py-2 rounded-full hover:bg-gray-300 shadow shadow-lg shadow-slate-800"
+                  className="bg-gradient-to-b from-slate-300 via-slate-100 to-slate-400 text-slate-600 px-4 py-2 rounded-full hover:bg-gray-300 shadow shadow-md shadow-slate-800"
                 >
                   GitHub Repository
                 </a>
