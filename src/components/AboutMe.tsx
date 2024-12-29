b9824094fe1d14a6b5e16a4d6b1e90c0d8d4ec0a
davidxv15 <davidxvaz@gmail.com>
2024-12-28 22:15:08 -0700

    oh my zsh

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,16 +1,29 @@
-import * as React from 'react';
+import * as React from "react";
 
 const AboutMe: React.FC = () => {
   return (
-    <section className="bg-gradient-to-b from-blue-300 via-sky-700 to-blue-300 text-zinc-900 tracking-tight py-12 h-screen px-4 sm:px-8" id="about">
+    <section
+      className="bg-gradient-to-b from-blue-300 via-sky-700 to-blue-300 text-zinc-900 tracking-tight py-12 h-screen px-4 sm:px-8"
+      id="about"
+    >
       <div className="max-w-4xl mx-auto text-center">
         <h2 className="text-5xl font-league mb-4 mt-4">About Me</h2>
         <p className="text-lg sm:text-xl leading-relaxed mb-8">
-        Hello,👋🏼 I'm David Velasquez, a versatile professional with a diverse background. As a Full Stack Web Developer, I specialize in creating engaging and effective user experiences with a strong emphasis on authentic design and brand recognition. I am also passionate about promoting differently-abled accessibility in my work. In addition to my technical skills, I have a rich array of interests and experiences. As a musician, I am a multi-instrumentalist who enjoys singing and recording music. I am also a former EMT and chef, and I continue to contribute to my community as a volunteer. This broad range of interests has helped me identify new opportunities for technology to make a positive impact.
+          Hello,👋🏼 I'm David Velasquez, a versatile professional with a diverse
+          background. As a Full Stack Web Developer, I specialize in creating
+          engaging and effective user experiences with a strong emphasis on
+          authentic design and brand recognition. I am also passionate about
+          promoting differently-abled accessibility in my work. In addition to
+          my technical skills, I have a rich array of interests and experiences.
+          As a musician, I am a multi-instrumentalist who enjoys singing and
+          recording music. I am also a former EMT and chef, and I continue to
+          contribute to my community as a volunteer. This broad range of
+          interests has helped me identify new opportunities for technology to
+          make a positive impact.
         </p>
         <p className="text-md sm:text-lg leading-relaxed">
-          Outside of coding, I love music and model building. 
-          I’m always eager to learn and take on new challenges.
+          Outside of coding, I love music and model building. I’m always eager
+          to learn and take on new challenges.
         </p>
       </div>
     </section>
