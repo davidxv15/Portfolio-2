79061eee878a87a69d81dc5e7ab467d11bc4e396
davidxv15 <davidxvaz@gmail.com>
2024-12-28 22:18:31 -0700

    pauseOnHover:true

--- src/components/SkillsCarousel.tsx
+++ src/components/SkillsCarousel.tsx
@@ -40,6 +40,7 @@ const CustomArrow = ({ className, style, onClick }: any) => (
       fontSize: "32px",
       zIndex: 10,
       fade: true,
+      pauseOnHover: true,
     }}
     onClick={onClick}
   >
