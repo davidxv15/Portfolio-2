6c2bb64cf7d2a1d2935ef528a070e94ed021cdc0
davidxv15 <davidxvaz@gmail.com>
2024-12-31 00:05:55 -0700

    bg-gradient

--- src/components/SkillsCarousel.tsx
+++ src/components/SkillsCarousel.tsx
@@ -82,7 +82,7 @@ const SkillsCarousel: React.FC = () => {
   return (
     <section className="relative w-auto max-w-sm lg:max-w-4xl mx-auto py-8">
       <h1 className="text-center p-8 md:p-2 md:pb-8 md:-mt-28" id="skills">Skills</h1>
-      <div className="bg-gradient-to-r from-sky-600 from-10% via-sky-900 via-50% to-sky-600 to-90% rounded-3xl shadow-none p-1">
+      <div className="bg-gradient-to-r from-sky-600 from-20% via-sky-100 via-50% to-sky-600 to-80% rounded-3xl shadow-none p-1">
         <Slider {...settings}>
           {logos.map((logo, index) => (
             <div
