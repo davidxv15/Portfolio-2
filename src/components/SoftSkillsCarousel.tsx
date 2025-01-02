9b652814cbcf5fe8d93ff4467186c15c51cead48
davidxv15 <davidxvaz@gmail.com>
2025-01-01 23:43:46 -0700

    w-60%

--- src/components/SoftSkillsCarousel.tsx
+++ src/components/SoftSkillsCarousel.tsx
@@ -25,7 +25,7 @@ const SoftSkillsCarousel: React.FC = () => {
   }, []);
 
   return (
-    <section className="relative w-full h-96 overflow-hidden bg-gradient-to-b from-sky-600 to-gray-600">
+    <section className="relative w-60% h-96 overflow-hidden bg-gradient-to-b from-sky-600 to-gray-600">
       <h2 className="text-center text-3xl text-white font-poppins mb-8">
         Soft Skills
       </h2>
