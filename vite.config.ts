921d95f4e924801c63b4be8103c22e6885480dbf
davidxv15 <davidxvaz@gmail.com>
2025-01-01 23:11:12 -0700

    server: open: false

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true, // auto open browser
+    open: false, // auto open browser
   },
 });
