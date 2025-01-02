35131674dbedf027ebc9d7e7231d834102310532
davidxv15 <davidxvaz@gmail.com>
2025-01-01 23:37:06 -0700

    red accents

--- src/components/SoftSkillsCarousel.tsx
+++ src/components/SoftSkillsCarousel.tsx
@@ -46,7 +46,7 @@ const SoftSkillsCarousel: React.FC = () => {
           {softSkills.map((skill, index) => (
             <div
               key={index}
-              className="absolute w-30 h-20 bg-gradient-to-r from-sky-500 to-sky-300 shadow-lg text-white text-center p-4 rounded-lg"
+              className="absolute w-30 h-20 bg-gradient-to-r from-red-500 to-sky-300 shadow-lg text-white text-center p-4 rounded-lg"
               style={{
                 transform: `rotateY(${index * 72}deg) translateZ(300px)`,
                 backfaceVisibility: "hidden",
