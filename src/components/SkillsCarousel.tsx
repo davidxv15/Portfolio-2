1695bfadbf7a6e4249bd63c0726422bd628cd3eb
davidxv15 <davidxvaz@gmail.com>
2025-01-12 23:44:50 -0700

    slidesToScroll: 5

--- src/components/SkillsCarousel.tsx
+++ src/components/SkillsCarousel.tsx
@@ -58,7 +58,7 @@ const SkillsCarousel: React.FC = () => {
     draggable: true,
     infinite: true,
     slidesToShow: 5,
-    slidesToScroll: 20,
+    slidesToScroll: 5,
     swipeToSlide: true,
     // dots: true,
     pauseOnHover: true,
