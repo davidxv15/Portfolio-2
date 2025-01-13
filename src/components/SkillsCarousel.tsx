d92b19852146ce86fc0fbfe05cb23be6a6405a1d
davidxv15 <davidxvaz@gmail.com>
2025-01-13 16:08:34 -0700

    breakpoints

--- src/components/SkillsCarousel.tsx
+++ src/components/SkillsCarousel.tsx
@@ -70,7 +70,7 @@ const SkillsCarousel: React.FC = () => {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 4,
         },
       },
       {
@@ -112,7 +112,7 @@ const SkillsCarousel: React.FC = () => {
               }}
             >
               <div
-                className="imgAndH3 smallCard bg-gradient-to-b from-slate-400 via-slate-300 to-slate-600 shadow-md shadow-gray-700 sm:w-24 sm:h-44 md:w-20 md:h-32 lg:w-48 lg:h-44"
+                className="imgAndH3 smallCard bg-gradient-to-b from-slate-400 via-slate-300 to-slate-600 shadow-md shadow-gray-700 m-1 sm:w-24 sm:h-44 md:w-32 md:h-32 lg:w-48 lg:h-44"
                 style={{
                   border: "2px solid #64748b",
                   // backgroundColor: "#0f172a",
