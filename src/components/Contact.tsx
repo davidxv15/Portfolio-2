7b5ad4f12b835608aed09469108b5ad1174a8ba1
davidxv15 <davidxvaz@gmail.com>
2025-01-18 17:22:50 -0700

    mt-14

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,7 +4,7 @@ const Contact: React.FC = () => {
   return (
     <section className="bg-gradient-to-b from-slate-900 to-sky-800 h-screen py-12" id="contact">
       <div className="max-w-4xl mx-auto text-center px-4">
-        <h2 className="text-3xl md:text-5xl font-bold text-white scroll-mt-4 font-poppins mb-6 mt-12">Get in Touch</h2>
+        <h2 className="text-3xl md:text-5xl font-bold text-white scroll-mt-4 mb-6 mt-14">Get in Touch</h2>
         <p className="text-white text-lg mb-8">
           How can I multiply your productivity?
         </p>
