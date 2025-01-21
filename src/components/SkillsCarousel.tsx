e2911b9936795c4d2b2203e472d754a22c6825f8
davidxv15 <davidxvaz@gmail.com>
2025-01-20 22:04:01 -0700

    lg:48

--- src/components/SkillsCarousel.tsx
+++ src/components/SkillsCarousel.tsx
@@ -132,7 +132,7 @@ const SkillsCarousel: React.FC<{ reverse?: boolean }> = ({ reverse = false }) =>
               }}
             >
               <div
-                className="imgAndH3 smallCard bg-gradient-to-b from-slate-400 via-slate-300 to-slate-600 shadow-md shadow-gray-700 m-1 sm:w-24 sm:h-44 md:h-44 md:w-40 lg:w-48 lg:h-44"
+                className="imgAndH3 smallCard bg-gradient-to-b from-slate-400 via-slate-300 to-slate-600 shadow-md shadow-gray-700 m-1 sm:w-24 sm:h-44 md:h-44 md:w-40 lg:w-48 lg:h-48"
                 style={{
                   border: "2px solid #64748b",
                   // backgroundColor: "#0f172a",
