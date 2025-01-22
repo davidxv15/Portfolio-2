ddba35996422225fe53ae6f053fc850610e6bad3
davidxv15 <davidxvaz@gmail.com>
2025-01-22 15:46:33 -0700

    bio edit

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -11,19 +11,13 @@ const AboutMe: React.FC = () => {
           About Me
         </h2>
         <p className="text-lg sm:text-xl leading-relaxed mb-8">
-          {/* Software Developer with hands-on problem-solving, creativity, and collaboration experience across diverse technical teams. I bring a strong sense of initiative, consistently transforming vision into action to deliver innovative solutions to complex challenges. My experience includes building responsive web applications with React and TypeScript, where I excel at delivering high-quality results under tight deadlines. With a laser-sharp eye for clean, maintainable code, I enjoy driving projects from conception to deployment, ensuring every detail aligns with the larger picture.
+        Software Developer with hands-on problem-solving, creativity, and collaboration experience across diverse technical teams. I bring a strong sense of initiative, consistently transforming vision into action to deliver innovative solutions to complex challenges. My experience includes building responsive web applications with React and TypeScript, where I excel at delivering high-quality results under tight deadlines. With a laser-sharp eye for clean, maintainable code, I enjoy driving projects from conception to deployment, ensuring every detail aligns with the larger picture.
 
-Beyond technical expertise, I excel at building personal connections and fostering professional relationships, creating an environment where collaboration and shared success thrive. Currently, I am charting a path into AI development, integrating machine learning models and exploring the fusion of AI and software engineering to build transformative, intelligent systems. Driven by the belief that bold ideas deserve decisive action, I thrive on creating impactful solutions that inspire progress. */}
-          Software Developer with hands on problem solving, creativity,
-          collaboration experience on a variety of technical teams. I bring
-          previous experience building responsive web applications with React
-          and TypeScript, often delivering high-quality solutions under tight
-          deadlines. I bring a laser sharp eye for creating clean, maintainable
-          code and enjoy driving projects from conception to deployment.
+Beyond technical expertise, I excel at building personal connections and fostering professional relationships, creating an environment where collaboration and shared success thrive. Currently, I am charting a path into AI development, integrating machine learning models and exploring the fusion of AI and software engineering to build transformative, intelligent systems. Driven by the belief that bold ideas deserve decisive action, I thrive on creating impactful solutions that inspire progress.
         </p>
         <p className="text-md sm:text-xl leading-relaxed">
-          Outside of coding, I love music, model building, and exploring. I’m always eager to learn and take on
-          new challenges.
+          Outside of coding, I love music, model building, and exploring. I’m
+          always eager to learn and take on new challenges.
         </p>
       </div>
     </section>
