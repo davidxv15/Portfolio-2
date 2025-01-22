c67f332b1e46f9a9cf799503a0fbda20851836ae
davidxv15 <davidxvaz@gmail.com>
2025-01-22 00:49:10 -0700

    spacing

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,7 +6,7 @@ const HeroSection: React.FC = () => {
       <div className="mb-2 bg-gradient-to-b from-blue-100 via-blue-100 to-blue-100">
         <h1
           className="surname text-5xl md:text-6xl lg:text-7xl font-league font-bold text-slate-900 bg-transparent 
-         w-3/4 mx-auto outline-none sm:-translate-y-12 md:-translate-y-4 lg:-translate-y-11"
+         w-3/4 mx-auto outline-none sm:-translate-y-12 md:-translate-y-4 lg:translate-y-20"
         >
           <span className="text-5.5xl sm:-translate-y-40 md:text-7xl lg:text-7xl">
             D
@@ -18,10 +18,10 @@ const HeroSection: React.FC = () => {
           ELASQUEZ
         </h1>
         <br />
-        <h2 className="text-5xl font-poppins text-slate-900 mt-1 md:text-left lg:text-center md:mr-32 md:ml-[50%] text-center lg:mr-16 lg:-translate-y-6 lg:w-90 md:whitespace-nowrap md:text-4xl lg:whitespace-nowrap">
+        <h2 className="text-5xl font-poppins text-slate-900 mt-1 md:text-left lg:text-center md:mr-32 md:ml-[50%] text-center lg:mr-16 lg:translate-y-20 lg:w-90 md:whitespace-nowrap md:text-4xl lg:whitespace-nowrap">
           Software Developer
         </h2>
-        <p className="text-xl md:text-justify lg:text-justify font-outfit text-slate-900 mt-2 md:text-left lg:text-left md:mr-24 md:ml-[50%] lg:ml-[54%] lg:-translate-y-1 animate-fade">
+        <p className="text-xl md:text-justify lg:text-justify font-outfit text-slate-900 mt-2 md:text-left lg:text-left md:mr-24 md:ml-[50%] lg:ml-[54%] lg:translate-y-32 animate-fade">
         Elevating team environments by leading with enthusiasm, integrity, and dedication. Driving success through actions that inspire
         {/* and motivate */}.
         </p>
@@ -30,10 +30,10 @@ const HeroSection: React.FC = () => {
         <img
           src="/WhiteShirtPhoto.jpg"
           alt="My Portrait"
-          className="rounded-full w-72 h-72 lg:w-96 lg:h-96 xl:w-106 xl:h-120 object-cover object-[80%_6%] shadow-lg shadow-gray-600 md:absolute md:top-40 md:left-24 lg:left-32 xl:left-60 xl:top-60 transition-all duration-900 ease-in-out animate-fade z-30 no-invert"
+          className="rounded-full w-72 h-72 lg:w-96 lg:h-96 xl:w-106 xl:h-120 object-cover object-[80%_6%] shadow-lg shadow-gray-600 md:top-40 md:left-24 lg:top-10 lg:left-20 xl:left-60 xl:top-60 transition-all duration-900 ease-in-out animate-fade z-30 no-invert"
         />
       </div>
-      <div className="mt-4 md:ml-64 md:mb-20 lg:space-x-12 md:translate-x-12 lg:translate-x-32 lg:translate-y-8 xl:translate-y-20">
+      <div className="mt-4 md:ml-64 md:mb-20 lg:space-x-12 md:translate-x-12 lg:translate-x-32 lg:-translate-y-36 xl:-translate-y-28">
         <button className="scroll-mt-20 bg-blue-900 text-white text-xl px-7 py-3 rounded-full hover:text-slate-900 hover:bg-blue-300 transition font-outfit shadow shadow-md shadow-gray-600 sm:-translate-x-0 md:-translate-y-20 lg:-translate-y-32 border-none transform transition duration-100 animate-fade">
           View My Work
         </button>
@@ -41,7 +41,7 @@ const HeroSection: React.FC = () => {
           Contact Me
         </button>
       </div>
-      <div className="flex space-x-4 mt-6 space-x-24 md:space-x-32 lg:space-x-36  md:translate-x-48 md:-translate-y-16 lg:-translate-y-20 lg:translate-x-64 xl:translate-y-1 animate-fade">
+      <div className="flex space-x-4 mt-6 space-x-24 md:space-x-32 lg:space-x-36  md:translate-x-48 md:-translate-y-16 lg:-translate-y-52 lg:translate-x-64 xl:-translate-y-32 animate-fade">
         <a
           href="https://github.com/davidxv15"
           target="_blank"
