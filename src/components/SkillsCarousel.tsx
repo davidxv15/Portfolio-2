2e9d3a12f2b4d3772e0c20d57de27d6444b91680
davidxv15 <davidxvaz@gmail.com>
2025-01-22 15:43:59 -0700

    caro space

--- src/components/SkillsCarousel.tsx
+++ src/components/SkillsCarousel.tsx
@@ -142,63 +142,66 @@ const SkillsCarousel: React.FC<{ tooltipText: string; reverse?: boolean }> = ({
         Skills
       </h1>
       {/* tooltip */}
-      <div className="relative group w-screen"><div className="absolute top-4 left-1/2 transform -translate-x-1/2 -translate-y-10 bg-gray-800 text-white text-lg px-4 py-2 rounded-md shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10">
-    <h3 className="text-lg font-bold underline mb-1 text-center">Skills</h3>
-    <ul className="list-none text-left">
-      {logos.map((logo, index) => (
-        <li key={index}>{logo.name}</li>
-      ))}
-    </ul>
-  </div>
-      <div className=" rounded-3xl shadow-none w-screen z-10 pt-1 mb-4">
-        <Slider {...settings}>
-          {logos.map((logo, index) => (
-            <div
-              key={index}
-              className={`flex flex-col items-center justify-center m-1 z-10 transform transition-transform duration-500 ${
-                logo.rounded ? "rounded-full" : ""
-              }`}
-              style={{
-                borderRadius: logo.border,
-                // transform: "translateX(1rem)",
-                zIndex: "1",
-                // fade or full
-                // width: "fade",
-              }}
-            >
+      <div className="relative group w-screen">
+        <div className="absolute top-4 left-1/2 transform -translate-x-1/2 -translate-y-10 bg-gray-800 text-white text-lg px-4 py-2 rounded-md shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10">
+          <h3 className="text-lg font-bold underline mb-1 text-center">
+            Skills
+          </h3>
+          <ul className="list-none text-left">
+            {logos.map((logo, index) => (
+              <li key={index}>{logo.name}</li>
+            ))}
+          </ul>
+        </div>
+        <div className=" rounded-3xl shadow-none w-screen z-10 pt-1 mb-4">
+          <Slider {...settings}>
+            {logos.map((logo, index) => (
               <div
-                className="imgAndH3 smallCard bg-gradient-to-b from-slate-400 via-slate-300 to-slate-600 shadow-md shadow-gray-700 m-1 sm:w-44 sm:h-48 md:h-48 md:w-40 lg:w-48 lg:h-48"
+                key={index}
+                className={`flex flex-col items-center justify-center m-1 z-10 transform transition-transform duration-500 ${
+                  logo.rounded ? "rounded-full" : ""
+                }`}
                 style={{
-                  border: "2px solid #64748b",
-                  // backgroundColor: "#0f172a",
-                  transition: "transform .5s ease-in-out",
-                  borderRadius: ".75rem",
-                  padding: "1rem .8rem",
-                  // width: "16rem",
-                  // height: "14rem",
+                  borderRadius: logo.border,
+                  // transform: "translateX(1rem)",
+                  zIndex: "1",
+                  // fade or full
+                  // width: "fade",
                 }}
               >
-                <img
-                  src={logo.src}
-                  alt={logo.name}
-                  className="smallCard w-32 h-32 sm:w-28 sm:h-24 md:w-20 md:h-20 lg:w-20 lg:h-20 lg:mt-1 mx-auto border-black"
+                <div
+                  className="imgAndH3 smallCard bg-gradient-to-b from-slate-400 via-slate-300 to-slate-600 shadow-md shadow-gray-700 m-1 sm:w-44 sm:h-48 md:h-48 md:w-40 lg:w-48 lg:h-48"
                   style={{
-                    border: "1.5px solid maroon",
-                    // border: "1.5px solid #64748b",
-                    
-                    // // backgroundColor: "navy",
-                    // transition: "transform .5s ease-in-out",
-                    // borderRadius: "4%",
-                    // padding: "1rem"
+                    border: "2px solid #64748b",
+                    // backgroundColor: "#0f172a",
+                    transition: "transform .5s ease-in-out",
+                    borderRadius: ".75rem",
+                    padding: "1rem .8rem",
+                    // width: "16rem",
+                    // height: "14rem",
                   }}
-                />
-                <h3 className="text-center mt-5 text-2xl lg:mx-0 lg: font-outfit bg-transparent text-slate-900 font-medium tracking-tight">
-                  {logo.name}
-                </h3>
+                >
+                  <img
+                    src={logo.src}
+                    alt={logo.name}
+                    className="smallCard w-32 h-32 sm:w-28 sm:h-24 md:w-20 md:h-20 lg:w-20 lg:h-20 lg:mt-1 mx-auto border-black"
+                    style={{
+                      border: "1.5px solid maroon",
+                      // border: "1.5px solid #64748b",
+
+                      // // backgroundColor: "navy",
+                      // transition: "transform .5s ease-in-out",
+                      // borderRadius: "4%",
+                      // padding: "1rem"
+                    }}
+                  />
+                  <h3 className="text-center mt-5 text-2xl lg:mx-0 lg: font-outfit bg-transparent text-slate-900 font-medium tracking-tight">
+                    {logo.name}
+                  </h3>
+                </div>
               </div>
-            </div>
-          ))}
-        </Slider>
+            ))}
+          </Slider>
         </div>
       </div>
       <IconCarousel reverse={true} />
