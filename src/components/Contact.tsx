e8423b956b3ee963867a2ab52f4db931981bae6e
davidxv15 <davidxvaz@gmail.com>
2025-01-23 00:44:27 -0700

    xs: screens

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,10 +3,10 @@ import * as React from "react";
 const Contact: React.FC = () => {
   return (
     <section
-      className="bg-gradient-to-b from-slate-900 to-sky-800 h-screen"
+      className="bg-gradient-to-b from-slate-900 to-sky-800 h-screen xs:py-80"
       id="contact"
     >
-      <div className="max-w-4xl mx-auto text-center px-4 -translate-y-16">
+      <div className="max-w-4xl mx-auto text-center px-4">
         <h2 className="text-3xl md:text-5xl font-bold text-white scroll-mt-4 mb-6">
           Get in Touch
         </h2>
