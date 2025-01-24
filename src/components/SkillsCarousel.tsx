363e56b3360d22b33ed2ea590747c43b386f751c
davidxv15 <davidxvaz@gmail.com>
2025-01-24 15:16:17 -0700

    alt

--- src/components/SkillsCarousel.tsx
+++ src/components/SkillsCarousel.tsx
@@ -23,6 +23,7 @@ const logos = [
   {
     name: "Collaboration",
     src: "Collaborative.webp",
+    alt: "icon of handshake",
     rounded: true,
     border: "50%",
   },
@@ -136,12 +137,12 @@ const SkillsCarousel: React.FC<{ tooltipText: string; reverse?: boolean }> = ({
   return (
     <section className="relative w-auto h-screen mx-auto py-4 h-screen bg-sky-600 z-0">
       <h1
-        className="scroll-mt-24 text-center w-screen md:p-1 md:pb-1 font-poppins bg-gradient-to-t from-sky-600 via-sky-600 z-0 mt-28 mb-4 skew"
+        className="scroll-mt-24 text-center w-screen md:p-1 md:pb-1 font-poppins bg-gradient-to-t from-sky-600 via-sky-600 z-0 mt-24 mb-10 -translate-y-1.5"
         id="skills"
       >
         SKILLSET
       </h1>
-      <hr className="w-32 mx-auto -translate-y-4"></hr>
+      <hr className="w-32 mx-auto -translate-y-11"></hr>
       {/* tooltip */}
       <div className="relative group w-screen">
         <div className="absolute top-4 left-1/2 transform -translate-x-1/2 -translate-y-10 bg-gray-800 text-white text-lg px-4 py-2 rounded-md shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10">
