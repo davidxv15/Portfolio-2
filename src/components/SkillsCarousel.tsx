1acb987e2499602536a4ab6104ce9d188c5f736c
davidxv15 <davidxvaz@gmail.com>
2025-01-24 14:31:13 -0700

    mt-1

--- src/components/SkillsCarousel.tsx
+++ src/components/SkillsCarousel.tsx
@@ -145,7 +145,7 @@ const SkillsCarousel: React.FC<{ tooltipText: string; reverse?: boolean }> = ({
       {/* tooltip */}
       <div className="relative group w-screen">
         <div className="absolute top-4 left-1/2 transform -translate-x-1/2 -translate-y-10 bg-gray-800 text-white text-lg px-4 py-2 rounded-md shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10">
-          <h3 className="text-lg font-bold underline mb-1 text-center">
+          <h3 className="text-lg font-bold underline mt-1 mb-1 text-center">
             Skills
           </h3>
           <ul className="list-none text-left">
