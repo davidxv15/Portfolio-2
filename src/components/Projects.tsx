6cdbb2ee2f4ed73aaee445ca3c1f3de7e4b68085
davidxv15 <davidxvaz@gmail.com>
2025-01-27 17:51:11 -0700

    drop-shadow-lg

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -33,7 +33,7 @@ const Projects: React.FC = () => {
       className="bg-gradient-to-t from-slate-400 from-10% via-slate-100 via-50% to-sky-600 to-98% py-20 px-2"
       id="projects"
     >
-      <h2 className="-scroll-mt-20 text-5xl tracking-tight font-poppins text-slate-200 text-center mb-10 mt-5">
+      <h2 className="-scroll-mt-20 text-5xl tracking-tight font-poppins text-slate-200 text-center mb-10 mt-5  drop-shadow-lg">
         PROJECTS
       </h2>
       <hr className="w-32 mx-auto -translate-y-8"></hr>
