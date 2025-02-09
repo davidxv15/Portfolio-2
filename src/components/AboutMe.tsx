5399a6472944c9a84bc1f314898cda4f792822ce
davidxv15 <davidxvaz@gmail.com>
2025-02-09 02:15:50 -0700

    text-left

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -11,7 +11,7 @@ const AboutMe: React.FC = () => {
           ABOUT ME
         </h2>
         <hr className="w-32 mx-auto -translate-y-9"></hr>
-        <p className="text-xl leading-relaxed mb-8 indent-4 text-justify font-poppins text-slate-400">
+        <p className="text-xl leading-relaxed mb-8 indent-4 text-left font-poppins text-slate-400">
         <span className="font-bold">Software Developer</span> with experience in problem-solving, creativity, and expert collaboration across diverse technical teams. I excel in building responsive web applications using <span className="font-bold">React</span> and <span className="font-bold">TypeScript</span>, consistently delivering high-quality solutions under tight deadlines. I take pride in driving projects from concept to deployment, ensuring precision and alignment with the bigger picture. 
         <br /><p className="indent-4">
         Beyond technical skills, I bring a positive attitude and a talent for creating meaningful connections between clients and brands, building relationships that inspire trust and mutual success. Currently, I am charting a path into <span className="font-bold">AI development</span>, integrating machine learning models and exploring the fusion of AI and software engineering to build transformative, intelligent systems.
