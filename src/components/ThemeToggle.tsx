fcec0f8c59c77ae329b04cb416f667dfbec00593
davidxv15 <davidxvaz@gmail.com>
2025-02-13 22:09:14 -0700

    slate-300

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -28,7 +28,7 @@ const ThemeToggle: React.FC<ThemeToggleProps> = ({
 
   return (
     <div className="flex justify-end items-center bg-transparent mb-3">
-      <span className="ml-4 mr-1 font-medium text-slate-400">
+      <span className="ml-4 mr-1 font-medium text-slate-300">
         {themes[currentThemeIndex]}
       </span>
       <button
