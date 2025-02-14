c888ea66b96b5caae542be3ddd43e69486ac73e7
davidxv15 <davidxvaz@gmail.com>
2025-02-13 22:23:14 -0700

    feat(toggle): themes typo

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ const App: React.FC = () => {
       </div>
       <div className="mr-auto flex justify-end font-outfit bg-blue-100 animate-fade">
         <ThemeToggle
-          themes={["default", "grayscale", "sepia", "  🔒"]}
+          themes={["default", "grayscale", "sepia", "🔒"]}
           onThemeChange={(theme) => handleThemeChange(theme)}
         />
       </div>
