25602176da6f2454a569153894a5fa13ac416253
davidxv15 <davidxvaz@gmail.com>
2025-02-20 01:45:23 -0700

    refactor(blast): 2 + 5

--- src/components/ZBlaster.tsx
+++ src/components/ZBlaster.tsx
@@ -88,8 +88,8 @@ const ZBlaster: React.FC = () => {
   }, []);
 
   const handleShoot = () => {
-    const bulletX = player.x - 2;
-    const bulletY = player.y - PLAYER_SIZE / 2 + 10;
+    const bulletX = player.x;
+    const bulletY = player.y + PLAYER_SIZE / 2 - 5; // Lower the laser origin closer to the player
 
     setBullets((prev) => [
       ...prev,
