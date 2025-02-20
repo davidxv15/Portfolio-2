0d5eda174dc5ff81667338f69874df8e99da197d
davidxv15 <davidxvaz@gmail.com>
2025-02-20 02:37:24 -0700

    refactor(blast): 0, 2 - 5

--- src/components/ZBlaster.tsx
+++ src/components/ZBlaster.tsx
@@ -89,9 +89,7 @@ const ZBlaster: React.FC = () => {
 
   const handleShoot = () => {
     const bulletX = player.x;
-    // const bulletY = player.y + PLAYER_SIZE / 2 - 5; 
-    const bulletY = player.y - PLAYER_SIZE / 4;
-
+const bulletY = player.y - PLAYER_SIZE / 2 - 5;
 
     setBullets((prev) => [
       ...prev,
