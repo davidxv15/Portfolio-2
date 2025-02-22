10b64a2ada318b212e61b7796b0713d4726c3812
davidxv15 <davidxvaz@gmail.com>
2025-02-21 23:15:20 -0700

    refactor(blast): if (keysPressed) statement

--- src/components/ZBlaster.tsx
+++ src/components/ZBlaster.tsx
@@ -67,12 +67,15 @@ const ZBlaster: React.FC = () => {
         let newX = prev.x;
         let newY = prev.y;
 
-        if (keysPressed.current["w"]) newY = Math.max(0, prev.y - PLAYER_SPEED);
-        if (keysPressed.current["s"])
-          newY = Math.min(SCREEN_HEIGHT - PLAYER_SIZE + 10, prev.y + PLAYER_SPEED);
-        if (keysPressed.current["a"]) newX = Math.max(0, prev.x - PLAYER_SPEED);
-        if (keysPressed.current["d"])
-          newX = Math.min(SCREEN_WIDTH - PLAYER_SIZE + 10, prev.x + PLAYER_SPEED);
+        if (keysPressed.current["w"]) 
+  newY = Math.max(PLAYER_SIZE / 2, prev.y - PLAYER_SPEED);
+if (keysPressed.current["s"]) 
+  newY = Math.min(SCREEN_HEIGHT - PLAYER_SIZE / 2, prev.y + PLAYER_SPEED);
+if (keysPressed.current["a"]) 
+  newX = Math.max(PLAYER_SIZE / 2, prev.x - PLAYER_SPEED);
+if (keysPressed.current["d"]) 
+  newX = Math.min(SCREEN_WIDTH - PLAYER_SIZE / 2, prev.x + PLAYER_SPEED);
+
 
         return { x: newX, y: newY };
       });
