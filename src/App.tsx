a2e93b5d7d08e8f4324e5824a9bb6f21bcb7f697
davidxv15 <davidxvaz@gmail.com>
2025-03-10 18:47:09 -0700

    refactor(App): window.innerwidth >= 768 && to ThemeToggle

--- src/App.tsx
+++ src/App.tsx
@@ -27,11 +27,14 @@ const App: React.FC = () => {
         <Navbar />
       </div>
       <div className="mr-auto flex justify-end font-outfit bg-blue-100 animate-fade">
-        <ThemeToggle
-          themes={["default", "grayscale", "sepia", "locked"]}
-          onThemeChange={(theme) => handleThemeChange(theme)}
-        />
-      </div>
+  {window.innerWidth >= 768 && (
+    <ThemeToggle
+      themes={["default", "grayscale", "sepia", "locked"]}
+      onThemeChange={(theme) => handleThemeChange(theme)}
+    />
+  )}
+</div>
+
       <HeroSection />
       <SkillsCarousel tooltipText="Skills" />
       {/* <SoftSkillsCarousel /> */}
