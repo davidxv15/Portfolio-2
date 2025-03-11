830d95136a0e47bba496e46e87cbe5b606e3e590
davidxv15 <davidxvaz@gmail.com>
2025-03-11 03:36:22 -0700

    style(App): hidden rule won't work here

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ const App: React.FC = () => {
       <div className="sticky top-0 z-50 justify-between bg-opacity-0">
         <Navbar />
       </div>
-      <div className="mr-auto flex justify-end font-outfit bg-blue-100 animate-fade sm:hidden md:hidden">
+      <div className="mr-auto flex justify-end text-red-400 font-outfit bg-blue-100 animate-fade">
     <ThemeToggle
       themes={["default", "grayscale", "sepia", "locked"]}
       onThemeChange={(theme) => handleThemeChange(theme)}
