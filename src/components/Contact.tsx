0ccd2d4c77a10052217f5860bceead45f80af0f7
davidxv15 <davidxvaz@gmail.com>
2025-03-10 19:47:42 -0700

    refactor(Contact):  animate-hauntingPulse for all div

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -9,14 +9,14 @@ interface ContactProps {
 const Contact: React.FC<ContactProps> = ({ theme }) => {
   return (
     <section className="bg-gradient-to-b from-slate-900 from-20% via-sky-900 via-90% to-gray-700 to-100% h-screen xs:py-80">
-      <div className="max-w-4xl mx-auto text-center px-4">
+      <div className="max-w-4xl mx-auto text-center px-4 animate-hauntingPulse">
         <h2 className="text-3xl md:text-5xl font-bold text-white scroll-mt-4 mb-6 animate-fade">
           Get in Touch
         </h2>
         <p className="text-white text-lg mb-8" id="contact">
           {/* How can I multiply your productivity? */}
         </p>
-        <div className="flex flex-col md:flex-row justify-center items-center space-y-6 md:space-y-0 md:space-x-8 animate-hauntingPulse">
+        <div className="flex flex-col md:flex-row justify-center items-center space-y-6 md:space-y-0 md:space-x-8">
           {/* LinkedIn */}
           <a
             href="https://www.linkedin.com/in/david-velasquez-az/"
