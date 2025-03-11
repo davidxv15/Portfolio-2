85050fc3896b02397bef09158da6253ea8860ace
davidxv15 <davidxvaz@gmail.com>
2025-03-10 22:50:17 -0700

    refactor(Projects): import useState

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -27,23 +27,25 @@ const ThemeToggle: React.FC<ThemeToggleProps> = ({
   
 
   return (
-    <div className="flex justify-end items-center bg-transparent mb-3">
-      <span className="ml-4 mr-1 font-medium text-slate-300 p-2">
+    <div className="flex justify-end items-center bg-transparent mb-2">
+      <span className="ml-4 mr-1 font-medium text-slate-300 p-4">
         {themes[currentThemeIndex]}
       </span>
+      <div className="p-4 rounded-full hover:bg-gray-300/10 transition duration-200">
       <button
         onClick={handleToggle}
         className="relative w-18 h-6 bg-gray-200 mr-4 rounded-full shadow-inner mt-1 focus:outline-none border-blue-100 hover:border-white shadow shadow-gray-700"
         aria-label="Theme Toggle"
-      >
+        >
         <div
           style={{
             transform: `translateX(${currentThemeIndex * 16}px)`,
             top: ".1rem",
           }} // Dynamic position
-          className="toggle-dot absolute left-0 top-0 h-5 w-5 bg-slate-800 shadow-inner rounded-full shadow shadow-gray-300 transform transition-transform duration-900 ease-in-out"
-        ></div>
-      </button>
+          className="toggle-dot absolute left-0 top-0 h-5 w-5 bg-slate-800 shadow-inner rounded-full shadow shadow-gray-300 transform transition-transform duration-900 ease-in-out">
+        </div>
+       </button>
+      </div>
     </div>
   );
 };
