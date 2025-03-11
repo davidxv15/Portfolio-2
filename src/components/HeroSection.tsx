254d4ed6a0166038dd9125147df2b0066b365362
davidxv15 <davidxvaz@gmail.com>
2025-03-10 19:12:38 -0700

    refactor(hero): gradient checks

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -19,7 +19,7 @@ const HeroSection: React.FC = () => {
   };
   return (
     <section className="sm:pt-40 md:pt-20 lg:-pt-20 section flex flex-col items-center justify-evenly h-screen w-screen bg-gradient-to-t from-sky-600 via-blue-100 to-blue-100 text-center ">
-      <div className="bg-gradient-to-b from-blue-100 via-blue-100 to-blue-100">
+      <div className="bg-gradient-to-b from-blue-100 via-blue-100 to--600">
         <h1
           className="surname relative top-[-6rem] md:top-[-1rem] lg:top-[3rem] text-5xl md:text-6xl lg:text-7xl font-league font-bold text-slate-900 bg-transparent 
          w- mx-auto outline-none sm:-translate-y-4 md:-translate-y-12 lg:-translate-8 xl:-translate-4 mt-32 sm:mt-28 scroll-mt-16"
