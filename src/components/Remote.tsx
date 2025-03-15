2cd2671d26135071ca2105e52f31cb199f99177b
davidxv15 <davidxvaz@gmail.com>
2025-03-14 23:42:17 -0700

    style(Remote): blue to red-600

--- src/components/Remote.tsx
+++ src/components/Remote.tsx
@@ -78,7 +78,7 @@ const Remote: React.FC = () => {
       <motion.div
         animate={{ x, y, rotate: rotation }}
         transition={{ ease: "linear", duration: 0.1 }}
-        className="absolute w-24 h-16 bg-blue-600 rounded-lg flex items-center justify-center"
+        className="absolute w-24 h-16 bg-red-600 rounded-lg flex items-center justify-center"
       >
         {/*  Wheels */}
         <div className="w-6 h-6 bg-black rounded-full absolute -left-3 top-2"></div>
