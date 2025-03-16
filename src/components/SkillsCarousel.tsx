6b13833469a7a7a666121275fde852e1e1aa7a25
davidxv15 <davidxvaz@gmail.com>
2025-03-15 23:49:06 -0700

    refactor(App): deploy readt

--- src/components/SkillsCarousel.tsx
+++ src/components/SkillsCarousel.tsx
@@ -3,7 +3,6 @@ import Slider from "react-slick";
 import IconCarousel from "./IconCarousel";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
 const logos = [
