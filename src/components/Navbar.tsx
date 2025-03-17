7c72b9aa392a5c1d74a573c37c5f738924578556
davidxv15 <davidxvaz@gmail.com>
2025-03-16 18:30:46 -0700

    style(Nav): text-gray 200 & 900

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,25 +9,25 @@ const Navbar: React.FC = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-gray-100 text-xl font-outfit flex items-center justify-between h-14">
           <div className="flex items-center">
-            <a href="#" className="text-gray-100 hover:text-gray-300 text-2xl">
+            <a href="#" className="text-gray-900 hover:text-gray-300 text-2xl">
               David Velasquez
             </a>
           </div>
           {/* Desktop Navigation Links */}
           <div className="hidden md:flex space-x-5 tracking-tight text-2xl">
-            <a href="#" className="text-gray-100 hover:text-gray-400 no-select">
+            <a href="#" className="text-gray-200 hover:text-gray-400 no-select">
               Home
             </a>
-            <a href="#skills" className="text-gray-100 hover:text-gray-400 no-select">
+            <a href="#skills" className="text-gray-200 hover:text-gray-400 no-select">
               Skills
             </a>
-            <a href="#projects" className="text-gray-100 hover:text-gray-400 no-select">
+            <a href="#projects" className="text-gray-200 hover:text-gray-400 no-select">
               Projects
             </a>
-            <a href="#about" className="text-gray-100 hover:text-gray-400 no-select">
+            <a href="#about" className="text-gray-200 hover:text-gray-400 no-select">
               About
             </a>
-            <a href="#contact" className="text-gray-100 hover:text-gray-400 no-select">
+            <a href="#contact" className="text-gray-200 hover:text-gray-400 no-select">
               Contact
             </a>
           </div>
